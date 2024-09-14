f1bbd2a3577862cb61c2ac62533a9386a717c02a
vaaluvishnu5146 <vaaluvishnu5146@gmail.com>
2024-09-14 12:43:15 +0530

    Add Bootstrap:
    1. Added Bootstrap5 config
    2. Added Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,18 @@
-import { Link, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Contact from "./pages/Contact";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <h1>Header</h1>
-      <div>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/products"}>Products</Link>
-        <Link to={"/contact"}>Contact Us</Link>
-      </div>
+      <Navbar />
       <Routes>
         <Route Component={Home} path="/" />
         <Route Component={Products} path="/products" />
         <Route Component={Contact} path="/contact" />
       </Routes>
-      <h1>Footer</h1>
     </>
   );
 }
